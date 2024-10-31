f94e4798f4d328a8f80dbe5e873fef74e783239e
nareshgantala <nareshgantala@gmail.com>
2024-10-31 12:17:07 +0530

    fix

--- setup_web.tf
+++ setup_web.tf
@@ -12,9 +12,9 @@ data "aws_instances" "existing_instance" {
 resource "null_resource" "ansible_playbook" {
   provisioner "local-exec" {
     command = <<-EOF
-      ansible-playbook -i ${data.aws_instance.existing_instance.public_ip}, setup_ejs.yml -e "ansible_user=centos ansible_password=DevOps321"
+      ansible-playbook -i ${data.aws_instances.existing_instance.public_ip}, setup_ejs.yml -e "ansible_user=centos ansible_password=DevOps321"
     EOF
   }
 
-  depends_on = [data.aws_instance.existing_instance]
+  depends_on = [data.aws_instances.existing_instance]
 }
